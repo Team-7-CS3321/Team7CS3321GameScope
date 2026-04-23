9d188d3cb5f81fbf3a19ffb9f5aa9c5ada2cb471
slate <slatefuller@isu.edu>
2026-04-23 17:46:04 -0600

    resolved merge conflicts with main

--- backend/src/team7cs3321gamescope/main.py
+++ backend/src/team7cs3321gamescope/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-from team7cs3321gamescope.api import steam, combined
 from team7cs3321gamescope.core.config import STEAM_API_KEY, RAWG_API_KEY
 from fastapi.middleware.cors import CORSMiddleware
 from team7cs3321gamescope.api import steam, combined
-from team7cs3321gamescope.core.config import STEAM_API_KEY, RAWG_API_KEY
 
 
 app = FastAPI(title="Team 7 GameScope API")
